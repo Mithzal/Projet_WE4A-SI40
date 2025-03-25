88a403fc58d0c5f0e5ab57aaa856ef4a1058f6f1
Alex Ramallo <71142809+Alrax@users.noreply.github.com>
2025-03-25 15:30:04 +0100

    .php

---
+++ Projet/TableauBord.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+ <head>
+   <meta charset="utf-8">
+   <title> Tableau de Bord </title>
+   <link rel="stylesheet" type="text/css" href="Accueil.css"/>
+ </head>
+
+ <body>
+    <div class="menu">
+        <a href="Accueil.php">
+            <button class="logo-button">
+                <img src="../Image/logo_utbm.png" alt="Logo" class="logo">
+            </button>
+         </a>
+        <a href="RechercheCours.php"><button>Recherche de Cours</button></a>
+        <a href="MesCours.php"><button>Mes cours</button></a>
+        <a href="TableauBord.php"><button>Tableau de bord</button></a>
+        <div class="dropdown">
+          <button class="dropbtn">Sites de l'UTBM</button>
+          <div class="dropdown-content">
+            <a href="https://outlook.office.com/mail/">Outlook</a>
+            <a href="https://utbm.sharepoint.com/sites/Portail?sw=auth">My UTBM</a>
+            <a href="https://bibliotheque.utbm.fr/">Bibliotheque</a>
+            <a href="https://ae.utbm.fr/">AE</a>
+          </div>
+        </div>
+        <button class="right">Déconnexion</button>
+      </div>
+
+      <div class="content">
+        <p>En construction</p>
+      </div>
+ </body>
