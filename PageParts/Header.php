005339ba573805c3af19d195728d5b3ab2d20a9b
glide070 <118171679+glide070@users.noreply.github.com>
2025-03-25 16:02:58 +0100

    Rebase du css et ajout de header.php, permettant son utilisation partout !

---
+++ PageParts/Header.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<!-- La "tête" sert à définir des propriétés globales de la page -->
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="../Styles/Accueil.css"/>
+
+    <?php
+    $title = "Je suis le titre de la page";
+    switch (basename($_SERVER['PHP_SELF'])) {
+        case "RechercheCours.php":
+            $title = "Recherche de Cours";
+            break;
+        case "MesCours.php":
+            $title = "Mes Cours";
+            break;
+        case "TableauBord.php":
+            $title = "Tableau de bord";
+            break;
+        case "Accueil.php":
+            $title = "Moodle";
+            break;
+    }
+
+
+
+    ?>
+    <title><?php echo $title;?></title>
+</head>
+
+<body>
+<div class="menu">
+    <a href="../Projet/Accueil.php">
+        <button class="logo-button">
+            <img src="../Image/logo_utbm.png" alt="Logo" class="logo">
+        </button>
+    </a>
+    <a href="../Projet/RechercheCours.php"><button>Recherche de Cours</button></a>
+    <a href="../Projet/MesCours.php"><button>Mes cours</button></a>
+    <a href="../Projet/TableauBord.php"><button>Tableau de bord</button></a>
+    <div class="dropdown">
+        <button class="dropbtn">Sites de l'UTBM</button>
+        <div class="dropdown-content">
+            <a href="https://outlook.office.com/mail/">Outlook</a>
+            <a href="https://utbm.sharepoint.com/sites/Portail?sw=auth">My UTBM</a>
+            <a href="https://bibliotheque.utbm.fr/">Bibliotheque</a>
+            <a href="https://ae.utbm.fr/">AE</a>
+        </div>
+    </div>
+    <button class="profile-button right">AS</button>
+</div>
+
+</body>
+
+</html>
--- Projet/Accueil.php
+++ Projet/Accueil.php
@@ -1,33 +1,12 @@
 <!DOCTYPE html>
 <html lang="fr">
- <head>
-   <meta charset="utf-8">
-   <title> Moodle </title>
-   <link rel="stylesheet" type="text/css" href="Accueil.css"/>
-   <link rel="icon" type="image/x-icon" href="../Image/Moodle.ico"/>
- </head>
+
+
+ <?php
+    include("../PageParts/Header.php");
+ ?>
 
  <body>
-   <div class="menu">
-     <a href="Accueil.php">
-        <button class="logo-button">
-            <img src="../Image/logo_utbm.png" alt="Logo" class="logo">
-        </button>
-     </a>
-     <a href="RechercheCours.php"><button>Recherche de Cours</button></a>
-     <a href="MesCours.php"><button>Mes cours</button></a>
-     <a href="TableauBord.php"><button>Tableau de bord</button></a>
-     <div class="dropdown">
-       <button class="dropbtn">Sites de l'UTBM</button>
-       <div class="dropdown-content">
-         <a href="https://outlook.office.com/mail/">Outlook</a>
-         <a href="https://utbm.sharepoint.com/sites/Portail?sw=auth">My UTBM</a>
-         <a href="https://bibliotheque.utbm.fr/">Bibliotheque</a>
-         <a href="https://ae.utbm.fr/">AE</a>
-       </div>
-     </div>
-     <button class="profile-button right">AS</button>
-   </div>
 
    <div class="content">
     <p>En construction</p>
