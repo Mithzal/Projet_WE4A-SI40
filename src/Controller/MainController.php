c622518b87398183027cbff98ae7a03a5ee0a4e1
glide070 <118171679+glide070@users.noreply.github.com>
2025-04-14 14:16:54 +0200

    add accountanagement into symfony

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,24 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class MainController extends AbstractController
 {
-    #[Route('/main', name: 'app_main')]
-    public function index(): JsonResponse
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/MainController.php',
-        ]);
-    }
 
-    #[Route('/', name:'template')]
+    #[Route('/', name:'home')]
     public function template(): Response
     {
-        return $this->render("base.html.twig");
+        $data = [
+            'variableTitre'=> 'Home'
+        ];
+        return $this->render("base.html.twig", $data);
     }
 }
