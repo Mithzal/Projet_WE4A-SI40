a68ff7faee8b3557daf9ab18d617bc5af4a361fa
glide070 <118171679+glide070@users.noreply.github.com>
2025-04-14 15:05:39 +0200

    edit base.html.twig with correct routes

---
+++ public/Scripts/ProfileMenu.js
@@ -0,0 +1,95 @@
+const initialContent = document.getElementById("profilePopup").innerHTML;//on récupère le contenu initial de la popup pour pouvoir l'utiliser plus tard
+
+function openProfilePopup() {
+    document.getElementById("profilePopup").innerHTML = initialContent;
+    document.getElementById("profilePopup").style.display = "block";
+}
+
+function closeProfilePopup() {
+    document.getElementById("profilePopup").style.display = "none";
+    document.getElementById("profilePopup").innerHTML = initialContent; // Réinitialise le contenu de la popup
+}
+
+function editProfile() {//cette fonction vient écraser le contenu de la div avec l'id profilePopup
+    document.getElementById("profilePopup").innerHTML = `
+        <div class="popup-box">
+            <span class="close" onclick="closeProfilePopup()">&times;</span>
+            <h2>Modifier les informations de l'utilisateur</h2>
+            <form id="editProfileForm">
+                <label for="ProfilePicture">Photo de profil:</label>
+                <input type="file" id="ProfilePicture" name="ProfilePicture"><br><br>
+                <label for="Firstname">Nom:</label>
+                <input type="text" id="Firstname" name="Firstname" value="John"><br><br><!-- Il faudra modifier les values pour que ce soit celle de la bdd -->
+                <label for="Lastname">Prénom</label>
+                <input type="text" id="Lastname" name="Lastname" value="Doe"><br><br>
+                 <button type="button" class="popup-button" onclick="submitProfileForm()">Enregistrer</button>
+            </form>
+        </div>
+    `;
+}
+function submitProfileForm() {
+    var ProfilePicture = document.getElementById("ProfilePicture").value;
+    var Fname = document.getElementById("Firstname").value;
+    var Lname = document.getElementById("Lastname").value;
+    // Ajoutez ici le code pour enregistrer les nouvelles informations dans la bdd
+    alert("Nom: " + Fname + "\nPrénom : "+Lname+ "\nPhoto de profil: " + ProfilePicture);
+    closeProfilePopup();
+}
+
+function editPassWord(){
+    document.getElementById("profilePopup").innerHTML = `
+        <div class="popup-box">
+            <span class="close" onclick="closeProfilePopup()">&times;</span>
+            <h2>Modifier le mot de passe</h2>
+            <form id="editPassWordForm">
+                <label for="oldPassword">Ancien mot de passe:</label>
+                <input type="password" id="oldPassword" name="oldPassword"><br><br>
+                <label for="newPassword">Nouveau mot de passe:</label>
+                <input type="password" id="newPassword" name="newPassword"><br><br>
+                <button type="button" class="popup-button" onclick="submitPassWordForm()">Enregistrer</button>
+            </form>
+        </div>
+    `;
+}
+
+function submitPassWordForm() {
+    var oldPassword = document.getElementById("oldPassword").value;
+    var newPassword = document.getElementById("newPassword").value;
+    if (oldPassword === "" || newPassword === "") {
+        alert("Veuillez remplir tous les champs.");
+        return;
+    }
+    if (oldPassword === newPassword) {
+        alert("Le nouveau mot de passe doit être différent de l'ancien.");
+        return;
+    }
+    if (newPassword.length < 8) {
+        alert("Le mot de passe doit contenir au moins 8 caractères.");
+        return;
+    }
+    if (!/[A-Z]/.test(newPassword)) {
+        alert("Le mot de passe doit contenir au moins une lettre majuscule.");
+        return;
+    }
+    if (!/[a-z]/.test(newPassword)) {
+        alert("Le mot de passe doit contenir au moins une lettre minuscule.");
+        return;
+    }
+    // if (oldPassword == bdd ) // vérification du mot de passe
+    // {
+    //     alert("Le mot de passe est incorrect.");
+    //     return;
+    // }
+
+
+    // Ajoutez ici le code pour enregistrer les nouvelles informations dans la bdd
+    alert("Ancien mot de passe: " + oldPassword + "\nNouveau mot de passe: " + newPassword);
+    closeProfilePopup();
+}
+
+window.onclick = function(event) {// permet de fermer la popup si on clique en dehors de celle-ci
+    var popup = document.getElementById("profilePopup");//on récupère la popup
+    if (event.target === popup) {//si on clique pas  sur la popup
+        popup.style.display = "none";//on la ferme
+    }
+}
---
+++ src/Controller/MesCoursController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class MesCoursController extends AbstractController
+{
+    #[Route('/mes/cours', name: 'MesCours')]
+    public function index(): Response
+    {
+        return $this->render('mes_cours/index.html.twig', [
+            'controller_name' => 'MesCoursController',
+        ]);
+    }
+}
