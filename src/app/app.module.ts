d6e11cc8de5adc418a88ab1962f056e8089769cf
glide070 <118171679+glide070@users.noreply.github.com>
2025-06-03 17:17:12 +0200

    all basics pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
 import {AppRoutingModule} from "./app-routing.module";
 import { RechercheCoursComponent } from './recherche-cours/recherche-cours.component';
+import { MesCoursComponent } from './mes-cours/mes-cours.component';
+import { TableauDeBordComponent } from './tableau-de-bord/tableau-de-bord.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     HomeComponent,
-    RechercheCoursComponent
+    RechercheCoursComponent,
+    MesCoursComponent,
+    TableauDeBordComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
