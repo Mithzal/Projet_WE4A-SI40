78acc2ffa45186143daa23f2df264804845adffe
glide070 <118171679+glide070@users.noreply.github.com>
2025-06-03 17:24:58 +0200

    basic routing (exept profile)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,18 @@ import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./home/home.component";
 import {RechercheCoursComponent} from "./recherche-cours/recherche-cours.component";
+import {MesCoursComponent} from "./mes-cours/mes-cours.component";
+import {TableauDeBordComponent} from "./tableau-de-bord/tableau-de-bord.component";
+import {ProfileComponent} from "./profile/profile.component";
 
 
 const routes : Routes =[
   {path :"", component : HomeComponent},
-  {path :"recherche", component : RechercheCoursComponent }
+  {path :"recherche", component : RechercheCoursComponent },
+  {path :"mes-cours", component : MesCoursComponent },
+  {path :"tableau-de-bord", component : TableauDeBordComponent },
+  {path :"profile", component : ProfileComponent},
+
 ]
 
 @NgModule({
