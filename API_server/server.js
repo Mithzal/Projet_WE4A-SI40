d5da942d086cf2d70b79e6857f397bc352b104ee
glide070 <118171679+glide070@users.noreply.github.com>
2025-06-10 14:57:18 +0200

    add forums to API

--- API_server/server.js
+++ API_server/server.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 6666;
 const logsRouter = require('./routes/logs');
 const usersRouter = require('./routes/users');
 const uesRouter = require('./routes/ues');
+const forumsRouter = require('./routes/forums');
 
 app.use(express.json())
 
@@ -19,6 +20,7 @@ app.use(cors());
 app.use('/api/logs', logsRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/ues', uesRouter);
+app.use('/api/forums', forumsRouter);
 
 //server running status
 app.listen(PORT, () => {
