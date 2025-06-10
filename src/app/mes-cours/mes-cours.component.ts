14a03edc32c0af47f32c352f6c2b9bde1189de08
Maud Royer <maud.royer@utbm.fr>
2025-06-10 16:20:02 +0200

    home

--- src/app/mes-cours/mes-cours.component.ts
+++ src/app/mes-cours/mes-cours.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
+// MesCoursComponent is a component that displays a list of courses for the user.
 @Component({
   selector: 'app-mes-cours',
   templateUrl: './mes-cours.component.html',
