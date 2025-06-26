bb35fd3299b22904562004350b4ff9b16d2c8770
glide070 <118171679+glide070@users.noreply.github.com>
2025-06-26 15:14:03 +0200

    forum fonctionnels

--- src/app/forums/messages/messages.component.ts
+++ src/app/forums/messages/messages.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {ForumMessage} from "../../../models/forums.model";
+import {UsersService} from "../../services/users.service";
+import {auditTime} from "rxjs";
 
 @Component({
   selector: 'app-messages',
@@ -14,10 +16,29 @@ export class MessagesComponent implements OnInit {
     author : ''
   };
 
+  name : string = "unknown"
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(private UserService : UsersService) { }
+
+  // Méthode pour obtenir le nom de l'auteur du message
+  loadAuthorName() {
+    if (this.message.author) {
+      this.UserService.getNameById(this.message.author).subscribe(
+        (data : any) => {
+          console.log(data)
+          this.name = data.name || 'Unknown';
+        }
+      );
+    }
   }
 
+
+
+
+  ngOnInit(): void {
+    this.loadAuthorName()
+  }
+
+  protected readonly auditTime = auditTime;
 }
