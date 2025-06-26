2e5fc35194bb1fe6e016968af70d5304aecd1b9a
Thvthvr <152478646+Thvthvr@users.noreply.github.com>
2025-06-26 14:45:58 +0200

    Début logs

--- src/app/admin-page/create-ue/create-ue.component.ts
+++ src/app/admin-page/create-ue/create-ue.component.ts
@@ -1,54 +1,61 @@
-  import {Component, Output, EventEmitter, OnInit, Input} from '@angular/core';
-  import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-  import {UsersService} from "../../services/users.service";
-  import {UEsService} from "../../services/ues.service";
-  import {User} from "../../../models/user.model";
+import { Component, Output, EventEmitter, OnInit, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UsersService } from '../../services/users.service';
+import { UEsService } from '../../services/ues.service';
+import { User } from '../../../models/user.model';
+import { LogsService } from '../../logs.service';
 
-  @Component({
-    selector: 'app-create-ue',
-    templateUrl: './create-ue.component.html',
-    styleUrls: ['./create-ue.component.css']
-  })
-  export class CreateUeComponent {
-    ueForm!: FormGroup;
-    @Input() teachers : User[] = [];
+@Component({
+  selector: 'app-create-ue',
+  templateUrl: './create-ue.component.html',
+  styleUrls: ['./create-ue.component.css']
+})
+export class CreateUeComponent implements OnInit {
+  ueForm!: FormGroup;
+  @Input() teachers: User[] = [];
 
-    @Output() close = new EventEmitter<void>();
+  @Output() close = new EventEmitter<void>();
 
-    constructor(private fb: FormBuilder, private service: UEsService, private UserSrvice : UsersService) {
-      this.UserSrvice.getTeachers().subscribe(data =>{
-        this.teachers = data;
-      })
-    }
-    log(){
+  constructor(
+    private fb: FormBuilder,
+    private service: UEsService,
+    private UserService: UsersService,
+    private logsService: LogsService
+  ) {
+    this.UserService.getTeachers().subscribe(data => {
+      this.teachers = data;
+    });
+  }
 
-    }
+  ngOnInit() {
+    this.ueForm = this.fb.group({
+      code: ['', Validators.required],
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      credits: ['', [Validators.required, Validators.min(1)]],
+      instructorId: ['', Validators.required]
+    });
+  }
 
-    ngOnInit() {
-      this.ueForm = this.fb.group({
-        code: ['', Validators.required],
-        name: ['',Validators.required],
-        description: ['', Validators.required],
-        credits: ['', [Validators.required, Validators.min(1)]],
-        instructorId: ['', Validators.required]
+  onSubmit() {
+    if (this.ueForm.valid) {
+      this.service.addUe(this.ueForm.value).subscribe({
+        next: (response) => {
+          // Création du log après succès
+          const log = {
+            type: 'creation',
+            message: `UE créée ${response.code} ${new Date().toLocaleString()}`,
+            userId: JSON.parse(localStorage.getItem('user') || '{}').id,
+          };
+          this.logsService.addLog(log).subscribe();
+          this.closeForm();
+        }
       });
-    }
-    onSubmit() {
-      if (this.ueForm.valid) {
-        this.service.addUe(this.ueForm.value).subscribe({
-          next: (response) => {
-            console.log('UE créée avec succès', response);
-            this.closeForm();
-          },
-          error: (error) => {
-            console.error('Erreur lors de la création de l\'UE', error);
-          }
-        });
-        console.log(this.ueForm.value);
-      }
-    }
-
-    closeForm() {
-      this.close.emit();
+      console.log(this.ueForm.value);
     }
   }
+
+  closeForm() {
+    this.close.emit();
+  }
+}
